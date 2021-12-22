9aa36343edeb8ccd1e66329b1960d09a2ab681f1
ranjithab80 <84181219+ranjithab80@users.noreply.github.com>
2021-12-22 22:05:07 +0000

    Update HomePage.java

--- src/main/java/com/crm/qa/pages/HomePage.java
+++ src/main/java/com/crm/qa/pages/HomePage.java
@@ -1,8 +1,16 @@
 package com.crm.qa.pages;
 
 public class HomePage {
+    HomePage homepage;
+    int sub(int a,int b)
+    {
+        return c=a-b;
+    }
     public static void main(String [] args)
     {
+        homepage=new HomePage();
+        homepage.sub();
+        System.out.println(homepage.c);
         System.out.println("ishanth");
     }
 }
