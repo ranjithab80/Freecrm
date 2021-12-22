fdbdc7e179ebd249b9604c71d08e19748560eaca
Ashwin <ashwin.anaveri@hmcts.net>
2021-12-22 21:55:15 +0000

    add method added in login page

--- src/main/java/com/crm/qa/pages/LoginPage.java
+++ src/main/java/com/crm/qa/pages/LoginPage.java
@@ -1,7 +1,15 @@
 package com.crm.qa.pages;
 
 public class LoginPage {
+    private static LoginPage loginpage;
+    int c;
+    int add(int a,int b)
+    {
+       return c=a+b;
+    }
     public static void main(String[] args) {
         System.out.println("Ranjitha");
+        loginpage.add(1,2);
+        System.out.println(loginpage.c);
     }
 }
